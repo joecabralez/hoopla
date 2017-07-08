e2392bee0059e8ffd86b51cb2eeba5297e0c3110
Joe <cabralez07@gmail.com>
2017-07-08 11:29:31 -0500

    changed date to show last finals game played

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,7 +36,7 @@ console.log(previousDate);
 //ajax to get sports api data/////
 //////////////////////////////////
 function sportsData() {
-  var queryUrl = "https://www.mysportsfeeds.com/api/feed/pull/nba/2017-playoff/scoreboard.json?fordate=" + todayDate;
+  var queryUrl = "https://www.mysportsfeeds.com/api/feed/pull/nba/2017-playoff/scoreboard.json?fordate=" + "20170612";
   $.ajax({
     method: "GET",
     url: queryUrl,
